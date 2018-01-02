45bf0befb4640f9d6f6d16b9093a5b5d90212c7c
Tor Robinson <torrobinson@gmail.com>
2018-01-02 10:15:03 -0700

    Grid preview in the console now uses unicode characters for readability

--- PicrossPreview/Program.cs
+++ PicrossPreview/Program.cs
@@ -36,12 +36,36 @@ namespace PicrossPreview
         {
             Console.Clear();
 
+            string pipe = "\u2502"; //│
+            string middleLine = "\u2500"; //━
+            string topLeft = "\u250C"; //┌
+            string topRight = "\u2510"; //┐
+            string bottomLeft = "\u2514";//└
+            string bottomRight = "\u2518"; //┘
+            string leftIntersection = "\u251C"; //├
+            string rightIntersection = "\u2524"; //┤
+            string topIntersection = "\u252C"; //┬
+            string bottomIntersection = "\u2534"; //┴
+            string intersection = "\u253C"; //┼
+
+
+            // Draw the top line
+            Console.WriteLine();
+            Console.Write(topLeft);
+            for (int i = 0; i < puzzle.Columns.Count * 2 + 1 - 2; i++)
+            {
+                Console.Write(i%2==0? middleLine : topIntersection);
+            }
+            Console.Write(topRight);
+            Console.WriteLine();
+
             // Lets just draw the first row of the puzzle for debug purposes
             foreach (Segment row in puzzle.Rows)
             {
                 bool first;
                 bool last;
                 int number = 1;
+
                 foreach (Cell cell in row.Cells)
                 {
                     first = number == 1;
@@ -49,15 +73,15 @@ namespace PicrossPreview
 
                     if (first)
                     {
-                        Console.Write("[" + GetCellCharacter(cell));
+                        Console.Write(pipe + GetCellCharacter(cell));
                     }
                     else if (last)
                     {
-                        Console.Write("|" + GetCellCharacter(cell) + "]");
+                        Console.Write(pipe + GetCellCharacter(cell) + pipe);
                     }
                     else
                     {
-                        Console.Write("|" + GetCellCharacter(cell));
+                        Console.Write(pipe + GetCellCharacter(cell));
                     }
 
                     number++;
@@ -72,8 +96,30 @@ namespace PicrossPreview
                     )
                 );
 
+                // Draw a line between each row (if not last line)
+                if (row != puzzle.Rows.Last())
+                {
+                    Console.WriteLine();
+                    Console.Write(leftIntersection);
+                    for (int i = 0; i < puzzle.Columns.Count * 2 - 1; i++)
+                    {
+                        Console.Write(i%2==0? middleLine : intersection);
+                    }
+                    Console.Write(rightIntersection);
+                }
+                
+
                 Console.WriteLine();
             }
+
+            // Draw LAST line
+            Console.Write(bottomLeft);
+            for (int i = 0; i < puzzle.Columns.Count * 2 + 1 - 2; i++)
+            {
+                Console.Write(i % 2 == 0 ? middleLine : bottomIntersection);
+            }
+            Console.Write(bottomRight);
+            Console.WriteLine();
 
             // Draw column sequences on bottom
             foreach (Segment column in puzzle.Columns)
@@ -108,15 +154,15 @@ namespace PicrossPreview
         {
             if (cell.IsTrue)
             {
-                return "#";
+                return "\u2588"; // █
             }
 
             if (cell.IsFalse)
             {
-                return "x";
+                return "x"; //x
             }
 
-            return " ";
+            return "⋅";
         }
     }
 }
